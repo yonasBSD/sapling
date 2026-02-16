79fda053a77dd6d54da71fab83c23e940b59b459
Luisa Vasquez Gomez <lmvasquezg@meta.com>
2026-02-16 02:55:35 -0800

    mononoke: add simple buck setup for testing restricted paths
    Summary: WTTS, I'm interested in seeing how this will work
    
    Reviewed By: Juzley
    
    Differential Revision: D92971127
    
    fbshipit-source-id: e792efae7bc21273fdfab6a8d61771fa0a8dc3af

---
+++ eden/mononoke/tests/mock_restricted_path/dummy_lib.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#include "eden/mononoke/tests/mock_restricted_path/dummy_lib.h"
+
+int get_restricted_value() {
+  return 42;
+}
---
+++ eden/mononoke/tests/mock_restricted_path/dummy_lib.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#pragma once
+
+int get_restricted_value();
