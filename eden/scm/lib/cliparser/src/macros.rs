d4beae57e513c08071145ceb9237040a86a782fa
Muir Manders <muirdm@meta.com>
2026-02-23 07:59:41 -0800

    cliparser: support positional args with default values
    Summary:
    Now Rust commands can easily support python commands like:
    
    ```
    def somecmd(ui, repo, somearg="default", **opts):
    ```
    
    with:
    
    ```
    struct SomeCmdOpts{
      #[arg]
      some_arg: string = "default",
    }
    ```
    
    Previously, Rust could only define the Vec<String> #[args], which required annoying manual checking/defaulting.
    
    Reviewed By: zzl0
    
    Differential Revision: D92236581
    
    fbshipit-source-id: 1453e686640fe4b73d79e96fa7ddc55d1fb3620b

--- eden/scm/lib/cliparser/src/macros.rs
+++ eden/scm/lib/cliparser/src/macros.rs
@@ -15,7 +15,7 @@ macro_rules! define_flags {
             args []
             varargs ()
             subflags []
-            misc ($vis $name 1 false)
+            misc ($vis $name 1 false 0)
         ); )*
     };
 }
@@ -27,10 +27,10 @@ macro_rules! _define_flags_impl {
     ( input []
       flags [ $( ($short:literal, $field:ident, $doc:expr, $type:ty, $default:expr, $argtype:literal) )* ]
       arg0 ( $( $arg0:ident )? )
-      args [ $( ($arg:ident, $arg_index:tt) )* ]
+      args [ $( ($arg:ident, $arg_index:tt, $arg_default:expr) )* ]
       varargs ( $($varargs:ident)? )
       subflags [ $( ($subflag_name:ident, $subflag_type:ty) )* ]
-      misc ($vis:vis $name:ident $varargs_offset:tt $has_varargs:tt)
+      misc ($vis:vis $name:ident $varargs_offset:tt $has_varargs:tt $min_args:tt)
     ) => {
         $vis struct $name {
             $( #[doc=$doc] pub $field : $type , )*
@@ -56,10 +56,27 @@ macro_rules! _define_flags_impl {
             type Error = $crate::anyhow::Error;
 
             fn try_from(out: $crate::parser::ParseOutput) -> $crate::anyhow::Result<Self> {
+                // Validate that required args (None default) don't come after optional args (Some default).
+                // This is a configuration error that should be caught during development.
+                {
+                    let defaults: &[Option<&str>] = &[ $( $arg_default, )* ];
+                    let mut seen_optional = false;
+                    for default in defaults.iter() {
+                        if default.is_some() {
+                            seen_optional = true;
+                        } else if seen_optional {
+                            panic!("invalid positional args: required #[arg]s cannot come after optional #[args]s");
+                        }
+                    }
+                }
+
                 if !$has_varargs && out.args.len() > $varargs_offset {
                     return Err($crate::errors::InvalidArguments.into());
                 }
-                if !out.args.is_empty() && out.args.len() < $varargs_offset {
+                // Check if we have enough args for required positional parameters.
+                // Required args are at indices 1..$min_args+1 (index 0 is reserved for command name).
+                // Only check if we have required args ($min_args > 0).
+                if $min_args > 0 && out.args.len() < $min_args + 1 {
                     return Err($crate::errors::InvalidArguments.into());
                 }
 
@@ -67,7 +84,7 @@ macro_rules! _define_flags_impl {
                     $( $field : out.pick::<$type>(&stringify!($field).replace("r#", "").replace("_", "-")), )*
                     $( $varargs: out.args.get($varargs_offset..).map(|v| v.to_vec()).unwrap_or_default(), )?
                     $( $arg0: out.args.get(0).cloned().unwrap_or_default(), )?
-                    $( $arg: out.args[$arg_index].clone(), )*
+                    $( $arg: out.args.get($arg_index).cloned().or_else(|| $arg_default.map(ToString::to_string)).unwrap(), )*
                     $( $subflag_name: <$subflag_type>::try_from(out.clone_only_opts())?, )*
                 })
             }
@@ -252,16 +269,39 @@ macro_rules! _define_flags_impl {
       args [ $( $rest_args:tt )* ]
       varargs $varargs:tt
       subflags $subflags:tt
-      misc ($vis:vis $name:ident $varargs_offset:tt $has_varargs:tt)
+      misc ($vis:vis $name:ident $varargs_offset:tt $has_varargs:tt $min_args:tt)
     ) => {
         $crate::_define_flags_impl!(
             input [ $( $rest )* ]
             flags $flags
             arg0 $arg0
-            args [ ($arg_name, $varargs_offset) $( $rest_args )* ]
+            args [ $( $rest_args )* ($arg_name, $varargs_offset, None::<&str>) ]
             varargs $varargs
             subflags $subflags
-            misc ($vis $name ($varargs_offset + 1) $has_varargs)
+            misc ($vis $name ($varargs_offset + 1) $has_varargs ($min_args + 1))
+        );
+    };
+
+    // Match a field like:
+    //
+    //    #[arg]
+    //    name: String = "default",
+    ( input [ #[arg] $arg_name:ident : String = $default:literal, $($rest:tt)* ]
+      flags $flags:tt
+      arg0 $arg0:tt
+      args [ $( $rest_args:tt )* ]
+      varargs $varargs:tt
+      subflags $subflags:tt
+      misc ($vis:vis $name:ident $varargs_offset:tt $has_varargs:tt $min_args:tt)
+    ) => {
+        $crate::_define_flags_impl!(
+            input [ $( $rest )* ]
+            flags $flags
+            arg0 $arg0
+            args [ $( $rest_args )* ($arg_name, $varargs_offset, Some($default)) ]
+            varargs $varargs
+            subflags $subflags
+            misc ($vis $name ($varargs_offset + 1) $has_varargs $min_args)
         );
     };
 
@@ -297,7 +337,7 @@ macro_rules! _define_flags_impl {
       args $args:tt
       varargs ()
       subflags $subflags:tt
-      misc ($vis:vis $name:ident $varargs_offset:tt false)
+      misc ($vis:vis $name:ident $varargs_offset:tt false $min_args:tt)
     ) => {
         $crate::_define_flags_impl!(
             input [ $( $rest )* ]
@@ -306,7 +346,7 @@ macro_rules! _define_flags_impl {
             args $args
             varargs ( $varargs_name )
             subflags $subflags
-            misc ($vis $name $varargs_offset true)
+            misc ($vis $name $varargs_offset true $min_args)
         );
     };
 
@@ -378,6 +418,34 @@ mod tests {
 
             #[args]
             args: Vec<String>,
+        }
+
+        struct PositionalArgsWithDefaults {
+            /// some flag
+            flag: bool,
+
+            #[arg]
+            required_arg: String,
+
+            #[arg]
+            optional_arg1: String = "default1",
+
+            #[arg]
+            optional_arg2: String = "default2",
+        }
+
+        struct MultipleOptionalArgs {
+            /// some flag
+            verbose: bool,
+
+            #[arg]
+            first: String = "one",
+
+            #[arg]
+            second: String = "two",
+
+            #[arg]
+            third: String = "three",
         }
     }
 
@@ -472,5 +540,101 @@ mod tests {
         assert!(parsed.test_opts.foo);
         assert!(parsed.foo);
         assert_eq!(parsed.args, vec!["1", "6"]);
+    }
+
+    #[test]
+    fn test_positional_args_with_defaults() {
+        // Test with all args provided
+        let parsed = ParseOptions::new()
+            .flags(PositionalArgsWithDefaults::flags())
+            .parse_args(&["cmdname", "required", "opt1", "--flag", "opt2"])
+            .unwrap();
+        let parsed = PositionalArgsWithDefaults::try_from(parsed).unwrap();
+        assert_eq!(parsed.required_arg, "required");
+        assert_eq!(parsed.optional_arg1, "opt1");
+        assert_eq!(parsed.optional_arg2, "opt2");
+        assert!(parsed.flag);
+
+        // Test with only required arg and one optional
+        let parsed = ParseOptions::new()
+            .flags(PositionalArgsWithDefaults::flags())
+            .parse_args(&["cmdname", "required", "custom1"])
+            .unwrap();
+        let parsed = PositionalArgsWithDefaults::try_from(parsed).unwrap();
+        assert_eq!(parsed.required_arg, "required");
+        assert_eq!(parsed.optional_arg1, "custom1");
+        assert_eq!(parsed.optional_arg2, "default2");
+
+        // Test with only required arg
+        let parsed = ParseOptions::new()
+            .flags(PositionalArgsWithDefaults::flags())
+            .parse_args(&["cmdname", "required"])
+            .unwrap();
+        let parsed = PositionalArgsWithDefaults::try_from(parsed).unwrap();
+        assert_eq!(parsed.required_arg, "required");
+        assert_eq!(parsed.optional_arg1, "default1");
+        assert_eq!(parsed.optional_arg2, "default2");
+
+        // Test missing required arg fails
+        let parsed = ParseOptions::new()
+            .flags(PositionalArgsWithDefaults::flags())
+            .parse_args(&["cmdname"])
+            .unwrap();
+        assert!(PositionalArgsWithDefaults::try_from(parsed).is_err());
+    }
+
+    #[test]
+    fn test_all_optional_args() {
+        // Test with no args - all should use defaults
+        let parsed = ParseOptions::new()
+            .flags(MultipleOptionalArgs::flags())
+            .parse_args(&["cmdname"])
+            .unwrap();
+        let parsed = MultipleOptionalArgs::try_from(parsed).unwrap();
+        assert_eq!(parsed.first, "one");
+        assert_eq!(parsed.second, "two");
+        assert_eq!(parsed.third, "three");
+        assert!(!parsed.verbose);
+
+        // Test with some args
+        let parsed = ParseOptions::new()
+            .flags(MultipleOptionalArgs::flags())
+            .parse_args(&["cmdname", "a", "b"])
+            .unwrap();
+        let parsed = MultipleOptionalArgs::try_from(parsed).unwrap();
+        assert_eq!(parsed.first, "a");
+        assert_eq!(parsed.second, "b");
+        assert_eq!(parsed.third, "three");
+
+        // Test with all args
+        let parsed = ParseOptions::new()
+            .flags(MultipleOptionalArgs::flags())
+            .parse_args(&["cmdname", "x", "y", "z"])
+            .unwrap();
+        let parsed = MultipleOptionalArgs::try_from(parsed).unwrap();
+        assert_eq!(parsed.first, "x");
+        assert_eq!(parsed.second, "y");
+        assert_eq!(parsed.third, "z");
+    }
+
+    define_flags! {
+        struct InvalidArgOrder {
+            #[arg]
+            optional: String = "default",
+
+            #[arg]
+            required: String,
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "invalid positional args")]
+    fn test_required_arg_after_optional_panics() {
+        let parsed = ParseOptions::new()
+            .flags(InvalidArgOrder::flags())
+            .parse_args(&["cmdname", "a", "b"])
+            .unwrap();
+        let args = InvalidArgOrder::try_from(parsed).unwrap();
+        let (_, _) = (args.optional, args.required);
     }
 }
