65dbae4e75b2e1cfad583f2f11e47a965ee819b5
Jarrett Dai <jldai@meta.com>
2026-03-26 16:49:53 -0700

    gitrepocompat: introduce new crate for dotrepo compatibility
    Summary:
    Similar to `gitcompat` and `edenfs-client`, `gitrepocompat` will be implemented with capabilities to support .repo/.
    
    Unlike them, however, `gitrepocompat` currently doesn't need to run / shell out to the `repo` program.
    
    The first capability is to initialize `sl` dotdir inside `.repo`, implemented in the next diff.
    
    Reviewed By: quark-zju
    
    Differential Revision: D98177744
    
    fbshipit-source-id: 021f63fe22d13c40db4fe27b39c2e6fd425acd9a

---
+++ eden/scm/lib/gitrepocompat/src/lib.rs
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//! Repo tool compatibility
+//!
+//! This crate provides capabilities to support the
+//! [repo tool](https://gerrit.googlesource.com/git-repo) with `.repo/` identity.
+
+pub mod init;
