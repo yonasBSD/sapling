7f7a5af9ec9dd0322cf1c8697539a989bed7e9a0
Spencer Burakowski <sburakowski@meta.com>
2026-04-06 10:18:23 -0700

    Migrate fake_client.py to modern thrift-python types
    Summary:
    Replace `facebook.eden.ttypes as py_deprecated_eden_ttypes` with modern
    `eden.fs.service.eden.thrift_types` in the doctor test fake client.
    Remove TODO(T250577897) comment.
    
    Reviewed By: MichaelCuevas
    
    Differential Revision: D94300418
    
    fbshipit-source-id: 003fbb9a0111c3ac6ed673c7786125ee706cbea2

--- eden/fs/cli/doctor/test/lib/fake_client.py
+++ eden/fs/cli/doctor/test/lib/fake_client.py
@@ -12,8 +12,6 @@ from collections import defaultdict
 from pathlib import Path
 from typing import Dict, List, NamedTuple, Optional
 
-# TODO(T250577897): Migrate code in util.py to use modern thrift-python types
-import facebook.eden.ttypes as py_deprecated_eden_ttypes
 from eden.fs.service.eden.thrift_types import (
     DebugInvalidateRequest,
     DebugInvalidateResponse,
@@ -89,34 +87,21 @@ class FakeClient:
     def set_counter_value(self, counter: str, value: int) -> None:
         self._counter_values[counter] = value
 
-    def listMounts(self) -> List[py_deprecated_eden_ttypes.MountInfo]:
+    def listMounts(self) -> List[MountInfo]:
         result = []
         for mount in self._mount_table.mounts:
             mount_path = Path(os.fsdecode(mount.mount_point))
             client_name = mount_path.parts[-1]
             client_path = self._eden_dir / "clients" / client_name
 
-            # Handle both None and actual mount states to preserve original behavior
             mount_state = self._path_mount_state[mount.mount_point]
 
-            if mount_state is None:
-                # For None states (old edenfs), create py-deprecated MountInfo directly
-                # to preserve exact original behavior where state can be None
-                thrift_mount_info = py_deprecated_eden_ttypes.MountInfo(
-                    mountPoint=mount.mount_point,
-                    edenClientPath=os.fsencode(client_path),
-                    state=None,
-                )
-            else:
-                # For actual mount states, use thrift-python MountState and convert
-                python_mount_info = MountInfo(
-                    mountPoint=mount.mount_point,
-                    edenClientPath=os.fsencode(client_path),
-                    state=mount_state,
-                )
-                thrift_mount_info = python_mount_info._to_py_deprecated()
-
-            result.append(thrift_mount_info)
+            mount_info = MountInfo(
+                mountPoint=mount.mount_point,
+                edenClientPath=os.fsencode(client_path),
+                state=mount_state,
+            )
+            result.append(mount_info)
 
         return result
 
