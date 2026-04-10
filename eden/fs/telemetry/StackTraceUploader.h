0bbe8cc1b2d38664bf1872ccec6b7716ac9ea4e4
Chandhana Solainathan <chandhana@meta.com>
2026-04-10 10:46:50 -0700

    Add StackTraceUploader header
    Summary: Declare StackTraceUploader, a utility class for uploading stack traces to the edenfs-errors-stacktraces Manifold bucket. Provides static methods for key generation, URL formatting, and background uploads.
    
    Reviewed By: janezhang10
    
    Differential Revision: D96540031
    
    fbshipit-source-id: bb1130e1cfef05ec9783551e4c603704721f6dfa

---
+++ eden/fs/telemetry/StackTraceUploader.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#pragma once
+
+#include <string>
+
+namespace facebook::eden {
+
+/**
+ * Uploads stack traces to Manifold and returns a URL for viewing them.
+ */
+class StackTraceUploader {
+ public:
+  static constexpr const char* kBucket = "edenfs-errors-stacktraces";
+  static constexpr const char* kApiKey = "edenfs-errors-stacktraces-key";
+  static constexpr int kUploadTimeoutSeconds = 10;
+
+  /**
+   * Generate a unique Manifold key for a stack trace.
+   * Format: flat/{random_hex}
+   */
+  static std::string generateKey();
+
+  /**
+   * Convert a Manifold key to a viewable URL.
+   */
+  static std::string keyToUrl(const std::string& key);
+
+  /**
+   * Upload content to Manifold in background, return URL immediately.
+   * If the upload queue is full, returns an error message instead.
+   */
+  static std::string uploadToManifold(std::string content);
+};
+
+} // namespace facebook::eden
