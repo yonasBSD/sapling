155e8649dd5a07cdabefae45610c61c141ee0333
Chandhana Solainathan <chandhana@meta.com>
2026-04-13 15:42:59 -0700

    Add DaemonError event type for structured error logging
    Summary: Adds DaemonError, a TypelessEvent that serializes EdenErrorInfo fields into DynamicEvent for Scuba ingestion. Each optional field is only added when present.
    
    Reviewed By: Alramech
    
    Differential Revision: D97970107
    
    fbshipit-source-id: dc18334c33d07557733375ce535442fee7d0d7a4

---
+++ eden/fs/telemetry/DaemonError.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#pragma once
+
+#include "eden/common/telemetry/LogEvent.h"
+#include "eden/fs/telemetry/EdenErrorInfo.h"
+
+namespace facebook::eden {
+
+struct DaemonError : public TypelessEvent {
+  EdenErrorInfo info;
+
+  explicit DaemonError(EdenErrorInfo info) : info(std::move(info)) {}
+
+  void populate(DynamicEvent& event) const override {
+    event.addString("component", std::string(toString(info.component)));
+    event.addString("error_message", info.errorMessage);
+    if (info.exceptionType.has_value()) {
+      event.addString("exception_type", *info.exceptionType);
+    }
+    if (info.errorCode.has_value()) {
+      event.addInt("error_code", *info.errorCode);
+    }
+    if (info.errorName.has_value()) {
+      event.addString("error_name", *info.errorName);
+    }
+    if (info.stackTrace.has_value()) {
+      event.addString("stack_trace", *info.stackTrace);
+    }
+    if (info.clientCommandName.has_value()) {
+      event.addString("client_command_name", *info.clientCommandName);
+    }
+    if (info.inode.has_value()) {
+      event.addInt("inode", static_cast<int64_t>(*info.inode));
+    }
+    if (info.filePath.has_value()) {
+      event.addString("file_path", *info.filePath);
+    }
+    if (info.mountPoint.has_value()) {
+      event.addString("mount_point", *info.mountPoint);
+    }
+  }
+};
+
+} // namespace facebook::eden
