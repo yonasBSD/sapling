5a303d05ff8443dc47c226642a115075a5d05f04
Prashant Pal <prashantpal@meta.com>
2026-04-16 06:12:48 -0700

    Remove megarepo_panic_on_repo_not_found_error JK
    Summary:
    The `scm/mononoke:megarepo_panic_on_repo_not_found_error` JK controlled
    whether a repo-not-found error panics (to surface the issue loudly) or
    returns a request error. It has been fully rolled out to `true` since
    October 2024 (536 days). The panic behavior is now permanent for non-test
    repos (id != 0).
    
    Reviewed By: lmvasquezg
    
    Differential Revision: D100634011
    
    fbshipit-source-id: 0bc981ea55071d9095ab07f7b406f0763fb3b75b

--- eden/mononoke/megarepo_api/src/lib.rs
+++ eden/mononoke/megarepo_api/src/lib.rs
@@ -223,14 +223,7 @@ impl<R: MononokeRepo> MegarepoApi<R> {
             .await
             .map_err(MegarepoError::internal)?
             .ok_or_else(|| {
-                if repo_id.id() != 0 // special case for some tests
-                    && justknobs::eval(
-                        "scm/mononoke:megarepo_panic_on_repo_not_found_error",
-                        None,
-                        Some(&repo_id.to_string()),
-                    )
-                    .unwrap_or(false)
-                {
+                if repo_id.id() != 0 {
                     panic!("repo not found {}", repo_id)
                 } else {
                     MegarepoError::request(anyhow!("repo not found {}", repo_id))
