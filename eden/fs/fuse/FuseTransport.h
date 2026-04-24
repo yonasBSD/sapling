1c032500fbb9233e5d586b592cadd7cb0c62428c
Kaveh Ahmadi <kavehahmadi@meta.com>
2026-04-24 15:21:33 -0700

    Introduce FuseTransport interface
    Summary:
    Introduce FuseTransport, an abstract interface that encapsulates FUSE
    I/O operations: sending replies (sendRawReply, sendErrorReply), reading
    the INIT packet, running the request loop, and stopping it.
    
    This interface will have two implementations:
      - DevFuseTransport: wraps the existing read()/writev() on /dev/fuse
      - IoUringFuseTransport: io_uring-based transport (future)
    
    Pure interface definition — no callers yet.
    
    Reviewed By: genevievehelsel
    
    Differential Revision: D100464013
    
    fbshipit-source-id: a7b8b66251488d944f36b4ff9298f413ba1d237a

---
+++ eden/fs/fuse/FuseTransport.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#pragma once
+
+#ifndef _WIN32
+
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <cstddef>
+
+#include "eden/fs/third-party/fuse_kernel_linux.h"
+
+namespace facebook::eden {
+
+class FuseChannel;
+
+class FuseTransport {
+ public:
+  virtual ~FuseTransport() = default;
+
+  virtual const char* getName() const = 0;
+  virtual ssize_t readInitPacket(int fd, void* buf, size_t size) const = 0;
+  virtual void processSession(FuseChannel& channel) = 0;
+  virtual void replyError(
+      FuseChannel& channel,
+      const fuse_in_header& request,
+      int errorCode) const = 0;
+  virtual void
+  sendRawReply(FuseChannel& channel, const iovec iov[], size_t count) const = 0;
+};
+
+} // namespace facebook::eden
+
+#endif
